2ad84cd7f06e76d7942140d3d737d6822bc6c0b4
Daniel Morley <morleydan10@gmail.com>
2023-12-12 19:18:08 -0500

    solutions

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,22 @@
 # your code goes here!
+from time import sleep
+
+count = 10
+#  "HAPPY NEW YEAR!"
+
+def countdown (count):
+    while count > 0:
+        print(f'{count} SECOND(S)!')
+        count -= 1
+    print("HAPPY NEW YEAR!")
+
+def countdown_with_sleep (count):
+    while count > 0:
+        print(f'{count} SECOND(S)!')
+        count -= 1
+        sleep(1)
+    print("HAPPY NEW YEAR!")
+
+# countdown(count)
+# print(cheer)
+
